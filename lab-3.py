38b136d398d16d6ccfbba815ae639c94cf057913
Pk2414 <prasanna0022192@gmail.com>
2024-09-03 12:01:16 +0530

    Add files via upload

---
+++ lab-3.py
@@ -0,0 +1,12 @@
+em_details={
+    "name":["sk","pk","ck","vk","dk"],
+    "ages":[18,19,21,16,20],
+    "salary":[23000,25000,24000,20000,26000]
+}
+for k,v in em_details.items():
+    if k=="salary":
+        print(min(v))
+        print(max(v))
+        print(sum(v))
+        if v==25000:
+          print(index(v))
